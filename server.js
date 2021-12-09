3c3d0ac6fe29503a2d74914b224fa247f58afbc9
Lowella Fox <foxlo25@c02dqecjmd6m.uswin.ad.vzwcorp.com>
2021-12-08 21:51:58 -0600

    Added CRUD to Restaurant model, Menu model and Item model

--- server.js
+++ server.js
@@ -1,30 +1,90 @@
 const express = require('express')
 const path = require('path') //node native module
-const { MenuItems } = require('../restaurant')
-// const { Menu } = require('./models/Menu')
-// const { Restaurant } = require('./models/Restaurant')
-// const {Item} = require('./models/Item')
+
 const {Restaurant, Menu, Item} = require('./models/index') 
 
+//configure express app
 const app = express()
 const port = 8000
 
 //points toward folder of static files
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json())
 
-//GET method on /restaurants route returns all restaurants
+//Restaurant model
+//create new restaurant
+app.post('/restaurants', async(req, res) => {
+    //create a restaurant using the json object passed in the request body
+    let newRestaurant = await Restaurant.create(req.body)
+    
+    //send a response string
+    res.send(`Restaurant id: ${req.body.id} has successfully created!`)
+})
+
+//GET method on /restaurants route 
+//returns all restaurants
 app.get('/restaurants', async (req,res) => {
     //find all instances of the Model Restaurant
     const allRestaurants = await Restaurant.findAll()
-    //respond with allRestaurants as a json objeect
+
+    //respond with allRestaurants as a json object
     res.json(allRestaurants)
+})
+
+//returns specific instance of restaurant by id
+app.get('/restaurants/:id', async(req, res) => {
+    const thisRestaurant = await Restaurant.findByPk(req.params.id)
+    res.send(thisRestaurant)
+})
+
+//returns specific restaurant by name
+app.get('/restaurants-name/:name', async(req, res) => {
+    //find one specific instances of the restaurant model
+    const thisRestaurant = await Restaurant.findOne({
+        where: {
+            name: req.params.name
+        }
+    })
+
+    //respond with specific json object
+    res.json(thisRestaurant)
+})
+
+//update one Restaurant by id
+app.put('/restaurants/:id', async(req, res) => {
+    let updateRestaurant = await Restaurant.update(req.body, {
+        where: { id: req.params.id}
+    })
+    res.send(`Restaurant id: ${req.params.id} has successfully updated!`)
+})
+
+//delete specific restaurant
+app.delete('/restaurants/:id', async(req, res) => {
+    await Restaurant.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    res.send(`Restaurant id: ${req.params.id} has successfully deleted!`)
+})
+//END
+
+//Menu model
+//create new menu
+app.post('/menus', async(req, res) => {
+    //create a menu using the json object passed in the request body
+    let newMenu = await Menu.create(req.body) 
+
+    //send a respond string
+    res.send(`Menu id: ${req.body.id} has successfully created!`)
 })
 
 //GET method on /menus route returns all menus
 app.get('/menus', async (req,res) => {
     //find all instances of the Model Menu
     const allMenu = await Menu.findAll()
-    //respond with menus as a json objeect
+
+    //respond with menus as a json object
     res.json(allMenu)
 })
 
@@ -37,7 +97,7 @@ app.get('/menus/:id', async(req, res) => {
     res.json(thisMenu)
 })
 
-//resturn menu by title
+//return menu by title
 app.get('/menus-name/:name', async(req, res) => {
     //find one specific instances of the menu model
     const thisMenuTitle = await Menu.findOne({
@@ -45,17 +105,84 @@ app.get('/menus-name/:name', async(req, res) => {
             title: req.params.name
         }
     })
-    
 
     //respond with specific json object
     res.json(thisMenuTitle)
 })
 
+//update one menu by id
+app.put('/menus/:id', async(req, res) => {
+
+    let updatedMenu = await Menu.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.send(`Menu id: ${req.body.id} has successfully updated!`)
+})
+
+//delete one menu by id
+app.delete('/menus/:id', async(req, res) => {
+    await Menu.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    res.send(`Menu id: ${req.params.id} has successfully deleted!`)
+})
+
+//Item Model
+//create new Item
+app.post('/items', async(req,res) => {
+    //create an item using the  json object passed in the request body
+    await Item.create(req.body)
+
+    //respond string
+    res.send(`Item id: ${req.body.id} has successfully created!`)
+})
+
+//get method on /items route
+//return all menu items
 app.get('/items', async (req, res) => {
+    //find all instances of the item model
     const allItems = await Item.findAll()
 
     res.json(allItems)
 })
+
+//return specific item
+app.get('/items/:id', async (req, res) => {
+    //find one instance of the item model
+    const thisItem = await Item.findOne({ 
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.json(thisItem)
+})
+
+//update one menu item by id
+app.put('/items/:id', async(req, res) => {
+    let updateItem = await Item.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+    res.send(`Item id: ${req.params.id} has successfully updated!`)
+})
+
+//delete one menu item by id
+app.delete('/items/:id', async(req, res) => {
+    await Item.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    res.send(`Item id: ${req.params.id} has successfully deleted!`)
+})
+
 
 //join query 2 models
 app.get('/restmenu', async (req, res) => {
@@ -82,6 +209,7 @@ app.get('/restmenu/:id', async (req, res) => {
 
     res.json(thisRestMenu)
 })
+/////END
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`)
